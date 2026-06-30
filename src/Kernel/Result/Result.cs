c17e019f2b232e4cde3215f607ddac12a5760eb7
RealBlank / <selyutinvi@gmail.com>
2026-07-01 00:51:55 +0300

    rework result

--- src/Kernel/Result/Result.cs
+++ src/Kernel/Result/Result.cs
@@ -3,49 +3,33 @@
 public class Result<T, E>
 {
     private readonly T? _value;
+    private readonly E? _error;
+    private readonly bool _isSuccess;
 
-    public T? Value
+    public T? Value => IsSuccess ? _value : throw new ResultException(_error?.ToString() ?? "unkown");
+
+    public E? Error { get; }
+
+    public bool IsSuccess => _isSuccess;
+
+    public bool IsFailure => !IsFailure;
+
+    private Result(T? value, E? error)
     {
-        get
-        {
-            if (Error != null)
-                throw new ResultException(Error.ToString() ?? "unkown");
+        if (value is not null && error is not null)
+            throw new InvalidOperationException("Result cannot have both value and error");
 
-            return _value;
-        }
-    }
-
-    public ErrorWrapper<E>? Error { get; }
-
-    public bool IsError
-    {
-        get
-        {
-            return Error == null;
-        }
-    }
-
-    public Result(T value)
-    {
-        Error = null;
+        _error = error;
         _value = value;
+        _isSuccess = error is null;
     }
 
-    public Result(E error)
-    {
-        Error = new ErrorWrapper<E>(error);
-        _value = default;
-    }
 
-    public Result(T value, E error)
-    {
-        Error = new ErrorWrapper<E>(error);
-        _value = value;
-    }
+    public static Result<T, E> Ok(T value) => new(value, default);
+    public static Result<T, E> Err(E value) => new(default, value);
 
-    public class ErrorWrapper<T>(T value)
-    {
-        public T Value { get; set; } = value;
-    }
+    public TResult Match<TResult>(Func<T, TResult> onOk, Func<E, TResult> onErr) => IsSuccess ? onOk(_value!) : onErr(_error!);
+    public Result<TResult, E> Map<TResult>(Func<T, TResult> mapper) => IsSuccess ? Result<TResult, E>.Ok(mapper(_value!)) : Result<TResult, E>.Err(_error!);
+    public Result<TResult, E> Bind<TResult>(Func<T, Result<TResult, E>> binder) => IsSuccess ? binder(_value!) : Result<TResult, E>.Err(_error!);
 }
 
